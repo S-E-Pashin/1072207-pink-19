9a96d45cea3272028c7d2326854ced3e2000ebba
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-05-23 03:57:20 +0300

    Доп критерии финал.

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/{logo-*.svg,icon-*.svg}")
     .pipe(svgstore({
       inlineSvg: true
     }))
